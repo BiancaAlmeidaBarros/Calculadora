956d11fcbedaeef009deb00af29b22b74711aa48
Bianca de Almeida Barros <108773539+BiancaAlmeidaBarros@users.noreply.github.com>
2022-07-15 21:29:47 -0300

    Initial commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+public class Calculadora {
+
+    double soma(double n1, double  n2){
+        return n1+n2;
+    }
+    double sub(double n1, double n2){
+        return n1-n2;
+    }
+    double mult(double n1,double n2){
+        return n1*n2;
+    }
+    double div(double n1, double n2){
+        return n1/n2;
+    }
+}
